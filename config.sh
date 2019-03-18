abf6728160a577a716a3e34b9ac98d17c6744125
tanbushi <tanbushi@qq.com>
2019-03-18 11:26:33 +0800

    Update config.sh

--- config.sh
+++ config.sh
@@ -10,16 +10,16 @@ declare -A config=(
   ["mysql_root_password_test"]="123456"  # 测试站点数据库root密码
   ["mysql_password_test"]="654321"       # 测试站点数据库用户密码，用户名会自动生成
   ["mysql_mapped_port_test"]=13306       # 测试站点数据库管理端口映射，便于客户端管理
-  ["dns_test"]="myapitest.mszmj.com"     # 测试站点域名
+  ["dns_test"]="myapitest.xxxxxx.com"     # 测试站点域名
   # 测试站点 ssl 证书文件名配置
-  ["ssl_certificate_test"]="1_myapitest.mszmj.com_bundle.crt"
-  ["ssl_certificate_key_test"]="2_myapitest.mszmj.com.key"
+  ["ssl_certificate_test"]="1_myapitest.xxxxxx.com_bundle.crt"
+  ["ssl_certificate_key_test"]="2_myapitest.xxxxxx.com.key"
   
   ["prod_site_name"]="prod"              # 生产站点名称
   ["mysql_root_password_prod"]="123456"  # 生产站点数据库root密码
   ["mysql_password_prod"]="654321"       # 生产站点数据库用户密码，用户名会自动生成
   ["mysql_mapped_port_prod"]=23306       # 生产站点数据库管理端口映射，便于客户端管理
-  ["dns_prod"]="myapi.mszmj.com"         # 生产站点域名
+  ["dns_prod"]="myapi.xxxxxx.com"         # 生产站点域名
   # 生产站点 ssl 证书文件名配置
   ["ssl_certificate_prod"]="1_myapi.xxxxxx.com_bundle.crt"
   ["ssl_certificate_key_prod"]="2_myapi.xxxxxx.com.key"
